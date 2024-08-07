1fac14a07595cb1f62e38c817cc1fe96b3997a1b
vkalyvayut-roboty-a-ne-chelovek <148657431+vkalyvayut-roboty-a-ne-chelovek@users.noreply.github.com>
2024-08-07 14:22:04 +0300

    init

---
+++ text_label/main.py
@@ -0,0 +1,16 @@
+from miros import ActiveObject
+from miros import return_status, signals
+from miros import spy_on
+
+
+class Statechart(ActiveObject):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
